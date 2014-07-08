6003ea80cb99af02518c6ab32905ef56baedf53c
allenarango <ararango@Allens-MacBook-Air.local>
2014-07-07 20:29:32 -0500

    .js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
+  
   $('#addnew').mousedown(function() {
      $('#addnewbutton').hide();
      $('#addnewform').show();
   });
-  $( "typelistitem" ).on(function() {
-  	$(this).val(function() {
-    return val.alert('.val');
+
+  $( "#formlistitem" ).submit(function( event ) {
+  $('this').click(function() {
+    $('input#typelistitem').val("");
+		$('#list').append('<h3 class="listitem">' + $('input#typelistitem').val() + '</h3>');
+  		event.preventDefault();
+		});	
+	
+	});
+
   });
   /*
 	$('#list').append('<li>'+your stored value variable+'</li>');
 */
-})
+
 
